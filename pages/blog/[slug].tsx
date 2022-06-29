5371f1fc061040fdf5b5de69baba726ff0b7b028
Luca Zani <lucazani2002@gmail.com>
2022-06-29 22:15:51 +0200

    feat(blog): added typography plugin for tailwind

--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -3,7 +3,8 @@ import { allBlogPosts } from "contentlayer/generated";
 import type { BlogPost } from "contentlayer/generated";
 import type { GetStaticPaths, GetStaticProps } from "next";
 import type { CustomNextPage } from "../../lib/types";
-import BlogLayout from "../../layouts/blog";
+import Image from "next/image";
+import { parseISO, format } from "date-fns";
 
 type Props = {
   post: BlogPost;
@@ -13,9 +14,41 @@ const BlogPage: CustomNextPage<Props> = ({ post }) => {
   const Component = useMDXComponent(post.body.code);
 
   return (
-    <BlogLayout post={post}>
-      <Component />
-    </BlogLayout>
+    <article className="mx-auto mb-16 flex w-full max-w-2xl flex-col items-start justify-center">
+      <h1 className="mb-4 text-3xl font-bold tracking-tight text-black dark:text-white md:text-5xl">
+        {post.title}
+      </h1>
+      <div className="mt-2 flex w-full flex-col items-start justify-between md:flex-row md:items-center">
+        <div className="flex items-center">
+          <Image
+            alt="Luca Zani"
+            height={24}
+            width={24}
+            src="/images/profile.jpg"
+            className="rounded-full"
+          />
+          <p className="ml-2 text-sm text-gray-700 dark:text-gray-300">
+            {"Luca Zani / "}
+            {format(parseISO(post.publishedAt), "MMMM dd, yyyy")}
+          </p>
+        </div>
+        <p className="min-w-32 mt-2 text-sm text-gray-600 dark:text-gray-400 md:mt-0">
+          {post.readingTime.text}
+        </p>
+      </div>
+      <div className="dark:prose-dark prose mt-4 w-full max-w-none">
+        <Component />
+      </div>
+      <div className="mt-4 text-sm text-gray-700 dark:text-gray-300">
+        <a
+          href={`https://github.com/zaniluca/zaniluca.com/edit/main/data/blog/${post.slug}.mdx`}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Edit on Github
+        </a>
+      </div>
+    </article>
   );
 };
 

ace9de11d1849f90e596b363a6e7f10e1de935d3
Luca Zani <lucazani2002@gmail.com>
2024-08-26 23:44:03 +0200

    refactor: moved Signature to its own file

---
+++ src/components/KampaayLogo.tsx
@@ -0,0 +1,7 @@
+import type { SVGProps } from "react";
+const KampaayLogo = (props: SVGProps<SVGSVGElement>) => (
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 360 360" {...props}>
+    <path d="m210.96 156.27-69.77 135.58H105.4l22.66-41.54-49.27-94.05h36.5l30.39 61.86 29.31-61.86h35.96ZM193.45 116.72l32.15-67.84 26.37 27.69-27.35 53.15-31.17-13zM234.62 139.82l56.09-20.28 28.43 28.2L247.6 171l-12.98-31.18z" />
+  </svg>
+);
+export default KampaayLogo;
--- src/components/curriculum/sections/CurriculumWorkExperienceSection.tsx
+++ src/components/curriculum/sections/CurriculumWorkExperienceSection.tsx
@@ -1,16 +1,76 @@
 import EducationCard from "../EducationCard";
 import CurriculumSectionHeading from "../CurriculumSectionHeading";
+import { motion, type Variants } from "framer-motion";
+
+const wrapper: Variants = {
+  offscreen: {
+    opacity: 0,
+  },
+  onscreen: {
+    opacity: 1,
+    transition: {
+      duration: 0.8,
+      staggerChildren: 0.22,
+    },
+  },
+};
+
+const text: Variants = {
+  offscreen: {
+    opacity: 0,
+    x: -10,
+  },
+  onscreen: {
+    opacity: 1,
+    x: 0,
+    transition: {
+      duration: 0.6,
+      staggerChildren: 0.22,
+    },
+  },
+};
 
 const CurriculumWorkExperience = () => {
   return (
     <div className="my-8">
       <CurriculumSectionHeading name="work experience" />
       <div className="mt-8 grid gap-8 sm:mt-4">
-        <EducationCard
-          datespan="Mar 2022 ― Apr 2023"
-          name="Frontend Developer @ Kampaay"
-          description="Worked in a rapidly growing startup environment with SCRUM Agile methodologies. Built customer facing features in Vue, React and Typescript. Helped migrating to new technologies (Nuxt 2 to 3, Vuex to Pinia, ...)"
-        />
+        <motion.div
+          variants={wrapper}
+          initial="offscreen"
+          whileInView="onscreen"
+          className="grid gap-2"
+          viewport={{ once: true, amount: 0.5 }}
+        >
+          <motion.span
+            variants={text}
+            className="text-sm font-normal leading-5 text-gray-500"
+          >
+            Mar 2022 ― Apr 2023
+          </motion.span>
+          <motion.h4
+            variants={text}
+            className="text-lg font-medium leading-6 text-gray-900"
+          >
+            Frontend Developer @{" "}
+            <a
+              target="_blank"
+              href="https://www.kampaay.com/"
+              className="underline decoration-gray-400 decoration-dotted underline-offset-4 transition-colors hover:text-gray-500"
+            >
+              Kampaay
+            </a>
+          </motion.h4>
+          <motion.p
+            variants={text}
+            className="max-w-lg text-base font-normal leading-6 text-gray-500"
+          >
+            Worked in a rapidly growing startup environment with SCRUM Agile
+            methodologies. Built customer facing features in Vue, React and
+            Typescript. Helped migrating to new technologies (Nuxt 2 to 3, Vuex
+            to Pinia, ...)
+          </motion.p>
+        </motion.div>
       </div>
     </div>
   );

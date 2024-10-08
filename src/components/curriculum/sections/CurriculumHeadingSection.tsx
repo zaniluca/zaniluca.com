b1690da31d60dfb4d245765c65dcde1bbf5198ea
Luca Zani <lucazani2002@gmail.com>
2024-10-08 22:54:36 +0200

    refactor: seo improvements

--- src/components/curriculum/sections/CurriculumHeadingSection.tsx
+++ src/components/curriculum/sections/CurriculumHeadingSection.tsx
@@ -16,7 +16,7 @@ const CurriculumHeadingSection = () => {
         </h1>
         <p className="mt-4 max-w-screen-lg leading-relaxed text-gray-500">
           an Italian Frontend Developer specialized in React, Typescript, Swift,
-          and Python. I enjoy writing clean and maintainable code and staying up
+          and GoLang. I enjoy writing clean and maintainable code and staying up
           to date with the latest technology standards and best practices. I'm
           very passionate about programming, web graphics, and Cloud Computing
         </p>

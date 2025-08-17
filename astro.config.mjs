7535a514966a2eb2f62e04eb58c493d662785c36
Luca Zani <lucazani2002@gmail.com>
2025-08-17 19:20:41 +0200

    chore: cloudflare adapter for deploying

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
-import vercel from "@astrojs/vercel/static";
 import sitemap from "@astrojs/sitemap";
+
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://zaniluca.com",
   integrations: [react(), tailwind(), sitemap()],
   output: "static",
-  adapter: vercel(),
+  adapter: cloudflare(),
 });
